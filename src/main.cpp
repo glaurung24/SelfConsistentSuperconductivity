7fc8418dd522b8e51125c02bdd9fa5c57c48e552
Andreas Theiler <andreas.theiler@gmx.at>
2025-01-28 16:28:06 +0100

    Added soc and zeeman field

--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,9 @@
  *
  *  Basic example of self-consistent superconducting order-parameter for a 2D
  *  tight-binding model with t = 1, mu = -1, and V_sc = 2. Lattice with edges
- *  and a size of 20x20 sites.
+ *  and a size of 20x20 sites. Additionally, a Zeeman field can be set via Vz and Rashba-soc via alpha_x and alpha_y.
  *
- *  @author Kristofer Björnson
+ *  @author Andreas Theiler
  */
 
 #include "TBTK/Array.h"
@@ -45,24 +45,30 @@ using namespace Visualization::MatPlotLib;
 const complex<double> i(0, 1);
 
 //Lattice size
-const int SIZE_X = 80;
-const int SIZE_Y = 80;
+const int SIZE_X = 40;
+const int SIZE_Y = 10;
 
-//Order parameter. The two buffers are alternatively swaped by setting
-//deltaCounter = 0 or 1. One buffer contains the order parameter used in the
-//previous calculation, while the other is used to store the newly obtained
-//order parameter that will be used in the next calculation.
+//Order parameter. 
 Array<complex<double>> Delta({SIZE_X, SIZE_Y});
-unsigned int deltaCounter = 0;
 
 //Superconducting pair potential, convergence limit, max iterations, and initial guess
-const double V_sc = 0.;
-const double CONVERGENCE_LIMIT = 0.000001;
+// and other Parameters.
+const complex<double> mu = 0.0;
+const complex<double> t_x = 0.25;
+const complex<double> t_y = 0.5;
+const double V_sc = 3.2;
+const double alpha_x = 0.4321; //SOC in x
+const double alpha_y = 0.0;	//SOC in y
+const double Vz = 1.00; // Zeeman field strength
 const int MAX_ITERATIONS = 50;
+const double CONVERGENCE_LIMIT = 0.000001;
 const complex<double> DELTA_INITIAL_GUESS = 0.3 + 0.0*i;
-const bool PERIODIC_BC = true;
+const double DELTA_INITIAL_GUESS_RANDOM_WINDOW = 0.0;
+const bool PERIODIC_BC_X = false;
+const bool PERIODIC_BC_Y = false;
+const bool SELF_CONSISTENCY = false;
 const bool USE_GPU = true;
-const bool USE_MULTI_GPU = true;
+const bool USE_MULTI_GPU = false;
 
 
 bool selfConsistencyStep(Solver::Diagonalizer solver){
@@ -79,14 +85,13 @@ bool selfConsistencyStep(Solver::Diagonalizer solver){
 		for(unsigned int y = 0; y < SIZE_Y; y++){
 			for(unsigned spin = 0; spin < 2; ++spin){
 				// Gap equation
-				Delta[{x, y}] += 0.5*V_sc *(-1.+2*spin) *property_extractor.calculateExpectationValue(
+				Delta[{x, y}] += 0.5*V_sc *(-1.+2*spin) * property_extractor.calculateExpectationValue(
 					{x,y, (spin+1)%2, 1 },
 					{x,y, spin, 0 }
-				);
+				);;
 			}
 		}
 	}
-
 	//Calculate convergence parameter
 	double maxError = 0.;
 	for(unsigned int x = 0; x < SIZE_X; x++){
@@ -132,7 +137,7 @@ class DeltaCallback : public HoppingAmplitude::AmplitudeCallback{
 
 //Function responsible for initializing the order parameter
 void initDelta(){
-	const double rand_spread = 0.0*abs(DELTA_INITIAL_GUESS);
+	const double rand_spread = DELTA_INITIAL_GUESS_RANDOM_WINDOW*abs(DELTA_INITIAL_GUESS);
 	srand (static_cast <unsigned> (time(0)));
 	for(unsigned int x = 0; x < SIZE_X; x++){
 		for(unsigned int y = 0; y < SIZE_Y; y++){
@@ -146,59 +151,76 @@ void initDelta(){
 int main(int argc, char **argv){
 	//Initialize TBTK.
 	Initialize();
-
-	//Parameters.
-	complex<double> mu = 0.0;
-	complex<double> t = 1.0;
 	Timer::tick("One execution");
 	//Create model and set up hopping parameters.
 	Model model;
 	for(int x = 0; x < SIZE_X; x++){
 		for(int y = 0; y < SIZE_Y; y++){
 			for(int s = 0; s < 2; s++){
-				//Add hopping ampltudes corresponding to
-				//chemical potential.
-				model << HoppingAmplitude(
-					-mu,
-					{x, y, s, 0},
-					{x, y, s, 0}
-				);
-				model << HoppingAmplitude(
-					mu,
-					{x, y, s, 1},
-					{x, y, s, 1}
-				);
+				for(int ph = 0; ph < 2; ++ph){ // ph: particle-hole index 0: particle, 1: hole
+					//Add hopping ampltudes corresponding to
+					//chemical potential.
+					double tau_z = (1-2*ph);
+					model << HoppingAmplitude(
+						-mu*tau_z,
+						{x, y, s, ph},
+						{x, y, s, ph}
+					);
+					// Zeeman term
+					model << HoppingAmplitude(
+						Vz*tau_z*(1-2*s),
+						{x, y, s, ph},
+						{x, y, s, ph}
+					);
+					//Add hopping parameters corresponding to t.
+					if(x+1 < SIZE_X || PERIODIC_BC_X){
+						model << HoppingAmplitude(
+							-tau_z*t_x,
+							{(x+1)%SIZE_X, y, s, ph},
+							{x, y, s, ph}
+						) + HC;
+					}
+					if(y+1 < SIZE_Y || PERIODIC_BC_Y){
+						model << HoppingAmplitude(
+							-tau_z*t_y,
+							{x, (y+1)%SIZE_Y, s, ph},
+							{x, y, s, ph}
+						) + HC;
 
-				//Add hopping parameters corresponding to t.
-				if(x+1 < SIZE_X || PERIODIC_BC){
-					model << HoppingAmplitude(
-						-t,
-						{(x+1)%SIZE_X, y, s, 0},
-						{x, y, s, 0}
-					) + HC;
-					model << HoppingAmplitude(
-						t,
-						{(x+1)%SIZE_X, y, s, 1},
-						{x, y, s, 1}
-					) + HC;
+					}
 				}
-				if(y+1 < SIZE_Y || PERIODIC_BC){
-					model << HoppingAmplitude(
-						-t,
-						{x, (y+1)%SIZE_Y, s, 0},
-						{x, y, s, 0}
-					) + HC;
-					model << HoppingAmplitude(
-						t,
-						{x, (y+1)%SIZE_Y, s, 1},
-						{x, y, s, 1}
-					) + HC;
-				}
+				// Order parameter is set by a function called deltaCallback
 				model << HoppingAmplitude(
 					deltaCallback,
 					{x, y, (s+1)%2, 1},
 					{x, y, s, 0}
 				) + HC;
+				// SOC in x direction
+				if(x+1 < SIZE_X || PERIODIC_BC_X){
+					model << HoppingAmplitude(
+							alpha_x, 
+							{(x+1)%SIZE_X,y,(s+1)%2, 0}, 
+							{x,y,s, 0}
+						) + HC;
+					model << HoppingAmplitude(
+							-alpha_x,
+							{x,y,s, 1},
+							{(x+1)%SIZE_X,y,(s+1)%2, 1}
+						) + HC;
+				}
+				// SOC in y direction
+				if(y+1 < SIZE_Y || PERIODIC_BC_Y){
+					model << HoppingAmplitude(
+							i*alpha_y, 
+							{x,(y+1)%SIZE_Y,(s+1)%2, 0}, 
+							{x,y,s, 0}
+						) + HC;
+					model << HoppingAmplitude(
+							-i*alpha_y, 
+							{x,y,s, 1},
+							{x,(y+1)%SIZE_Y,(s+1)%2, 1}
+						) + HC;
+				}
 			}
 		}
 	}
@@ -206,30 +228,36 @@ int main(int argc, char **argv){
 	//Construct model
 	model.construct();
 
+	if(!model.isHermitian()){
+		cout << "Model not Hermitian"  << endl;
+		exit(0);
+	}
+
 	//Initialize D
 	initDelta();
 
 	//Setup and run Solver::Diagonalizer
 	Solver::Diagonalizer solver;
 	solver.setModel(model);
-	// solver.setMaxIterations(MAX_ITERATIONS);
-	// solver.setSelfConsistencyCallback(selfConsistencyCallback);
 	solver.setUseGPUAcceleration(USE_GPU);
 	solver.setUseMultiGPUAcceleration(USE_MULTI_GPU);
-	solver.setVerbose(true);
+	solver.setVerbose(false);
 	Streams::out << model.getBasisSize() << endl;
 	solver.run();
 	Timer::tock();
-	exit(0);
-	// Selfconsistency loop
-	for(int loop_counter = 0; loop_counter < MAX_ITERATIONS; ++loop_counter){
-		cout << "Sc loop nr: " << loop_counter << endl;
-		cout << Delta[{0,0}] << endl;
-		if(selfConsistencyStep(solver)){
-			break; // Exit loop if self consistency condition is achieved
+
+	if(SELF_CONSISTENCY){
+		// Selfconsistency loop
+		for(int loop_counter = 0; loop_counter < MAX_ITERATIONS; ++loop_counter){
+			cout << "Sc loop nr: " << loop_counter << endl;
+			cout << Delta[{0,0}] << endl;
+			if(selfConsistencyStep(solver)){
+				break; // Exit loop if self consistency condition is achieved
+			}
+			solver.run();
 		}
-		solver.run();
 	}
+
 
 	//Calculate abs(D) and arg(D)
 	Array<double> DeltaAbs({SIZE_X, SIZE_Y});
@@ -268,6 +296,12 @@ int main(int argc, char **argv){
 	dos = Smooth::gaussian(dos, SMOOTHING_SIGMA, SMOOTHING_WINDOW);
 	plotter.plot(dos);
 	plotter.save("../../figures/DOS.png");
+	plotter.clear();
+
+	Property::EigenValues ev = propertyExtractor.getEigenValues();
+	plotter.plot(ev.getData());
+	plotter.save("../../figures/Eigenvalues.png");
+	plotter.clear();
 
 
 
